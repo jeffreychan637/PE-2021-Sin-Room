61c19a86509d458f30a3640c7fd6e0d95d104526
Jeffrey Chan <jeffreychan@tcs-macbook-pro.lan>
2021-03-01 23:54:27 -0800

    fix init script

--- insertScripts.js
+++ insertScripts.js
@@ -4,7 +4,7 @@ const link = document.createElement('link');
 link.rel  = 'stylesheet';
 link.type = 'text/css';
 link.href = `https://cdn.jsdelivr.net/gh/jeffreychan637/PE-2021-Sin-Room@main/webflow-sin-room.css?version=${Math.random()}`;
-head.appendChild(script);
+head.appendChild(link);
 
 const script = document.createElement('script');
 script.type = 'text/javascript';

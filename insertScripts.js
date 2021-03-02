faf4467e72a926243ca15b6a37b3ac4984107ddc
Jeffrey Chan <jeffreychan@tcs-macbook-pro.lan>
2021-03-01 23:59:51 -0800

    fix init script

--- insertScripts.js
+++ insertScripts.js
@@ -3,10 +3,10 @@ const head = document.getElementsByTagName('head')[0];
 const link = document.createElement('link');
 link.rel  = 'stylesheet';
 link.type = 'text/css';
-link.href = `https://cdn.jsdelivr.net/gh/jeffreychan637/PE-2021-Sin-Room@main/webflow-sin-room.css?version=${Math.random()}`;
+link.href = `https://cdn.jsdelivr.net/gh/jeffreychan637/PE-2021-Sin-Room@latest/webflow-sin-room.css?version=${Math.random()}`;
 head.appendChild(link);
 
 const script = document.createElement('script');
 script.type = 'text/javascript';
-script.src = `https://cdn.jsdelivr.net/gh/jeffreychan637/PE-2021-Sin-Room@main/webflow-sin-room.js?version=${Math.random()}`;
+script.src = `https://cdn.jsdelivr.net/gh/jeffreychan637/PE-2021-Sin-Room@latest/webflow-sin-room.js?version=${Math.random()}`;
 head.appendChild(script);
